67035235220730bc88b0817ed258f291b8786941
sumithankarapu <82142000+sumithankarapu@users.noreply.github.com>
2021-10-04 13:32:59 +0530

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is a plant website where you can add plants and see different types of plants</h1>
   </div>
 </template>
